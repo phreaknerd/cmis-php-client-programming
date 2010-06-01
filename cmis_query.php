b632e9f3b2c52465e31cf7a342487555c5328dd5
richmck <richmck@localhost.localdomain>
2010-05-31 21:43:32 -0400

    Reformat and add Apache Licensing to all

--- cmis_query.php
+++ cmis_query.php
@@ -1,37 +1,61 @@
 <?php
-//  Author: Rich McKnight rich.mcknight@alfresco.com http://oldschooltechie.com
-require_once('cmis_repository_wrapper.php');
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#   Authors:
+#    Richard McKnight
+#
+
+require_once ('cmis_repository_wrapper.php');
 $repo_url = $_SERVER["argv"][1];
 $repo_username = $_SERVER["argv"][2];
 $repo_password = $_SERVER["argv"][3];
 $repo_debug = $_SERVER["argv"][4];
-   
-$client=new CMISService($repo_url,$repo_username,$repo_password);
 
-if ($repo_debug) {
-	print "Repository Information:\n===========================================\n";
-	print_r($client->workspace);
-	print "\n===========================================\n\n";
+$client = new CMISService($repo_url, $repo_username, $repo_password);
+
+if ($repo_debug)
+{
+    print "Repository Information:\n===========================================\n";
+    print_r($client->workspace);
+    print "\n===========================================\n\n";
 }
 
-$objs=$client->query("select * from cmis:folder");
-if ($repo_debug) {
-	print "Returned Objects\n:\n===========================================\n";
-	print_r($objs);
-	print "\n===========================================\n\n";
+$objs = $client->query("select * from cmis:folder");
+if ($repo_debug)
+{
+    print "Returned Objects\n:\n===========================================\n";
+    print_r($objs);
+    print "\n===========================================\n\n";
 }
 
-foreach ($objs->objectList as $obj) {
-	if ($obj->properties['cmis:baseTypeId'] == "cmis:document") {
-		print "Document: " . $obj->properties['cmis:name'] . "\n";
-	} elseif ($obj->properties['cmis:baseTypeId'] == "cmis:folder") {
-		print "Folder: " . $obj->properties['cmis:name'] . "\n";
-	} else {
-		print "Unknown Object Type: " . $obj->properties['cmis:name'] . "\n";
-	}
+foreach ($objs->objectList as $obj)
+{
+    if ($obj->properties['cmis:baseTypeId'] == "cmis:document")
+    {
+        print "Document: " . $obj->properties['cmis:name'] . "\n";
+    }
+    elseif ($obj->properties['cmis:baseTypeId'] == "cmis:folder")
+    {
+        print "Folder: " . $obj->properties['cmis:name'] . "\n";
+    } else
+    {
+        print "Unknown Object Type: " . $obj->properties['cmis:name'] . "\n";
+    }
 }
 
-if ($repo_debug > 2) {
-	print "Final State of CLient:\n===========================================\n";
-	print_r($client);
+if ($repo_debug > 2)
+{
+    print "Final State of CLient:\n===========================================\n";
+    print_r($client);
 }
